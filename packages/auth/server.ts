c207125f4242fe1da67fa08d3e72219e68278a5c
ephraimduncan <ephraimduncan68@gmail.com>
2026-01-26 11:27:28 +0000

    fix: handle React.cache unavailable in test environment

--- packages/auth/server.ts
+++ packages/auth/server.ts
@@ -1,7 +1,12 @@
-import { cache } from 'react';
+import * as React from 'react';
 import { headers } from 'next/headers';
 
 import { auth } from './index';
+
+const cache =
+  typeof React.cache === 'function'
+    ? React.cache
+    : <T extends (...args: unknown[]) => unknown>(fn: T) => fn;
 
 export const getSession = cache(async () => {
   return auth.api.getSession({ headers: await headers() });
